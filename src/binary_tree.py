735f7e24956e8886f44c7735e3eaf9778fa7d68a
samir belhadjer <sbelhadjer@gmail.com>
2021-12-28 18:27:45 +0100

    add some static length just for some test

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -1,5 +1,14 @@
+from __future__ import annotations
+from typing import Type, TypeVar
+
+
+Tree = TypeVar('T', bound='Tree')
 
 class Tree:
-    def __init__(self ,right=None,left=None):
+    def __init__(self : type(Tree) ,right : type(Tree)=None ,left : type(Tree)=None ) -> None:
         self.right = right 
         self.left = left 
+
+    @classmethod
+    def length(cls , root : Tree) -> int:
+        return 3
--- tests/test_explore.py
+++ tests/test_explore.py
@@ -12,3 +12,14 @@ class TestBinaryExplore:
         root.right = right 
 
         assert root.left == left
+    
+    def test_binary_length(self):
+
+        root = Tree()
+        left = Tree()
+        right = Tree()
+        
+        root.left = left 
+        root.right = right 
+
+        assert Tree.length(root) == 3
